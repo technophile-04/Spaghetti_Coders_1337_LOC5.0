40b725a70a5259e5e2aeeedac0ba6d286c44e97f
Aadil Saudagar <aadilsaudagar24@gmail.com>
2023-03-12 00:07:31 +0530

    Done Landing

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -2,6 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 
+// import Header from "../components/partials/Header";
+import HeroHome from "../components/partials/HeroHome";
+import FeaturesHome from "../components/partials/Features";
+import FeaturesBlocks from "../components/partials/FeaturesBlocks";
+import Testimonials from "../components/partials/Testimonials";
+import Newsletter from "../components/partials/Newsletter";
+// import Footer from "../components/partials/Footer";
+import Banner from "../components/partials/Banner";
+
 const Home: NextPage = () => {
   return (
     <>
@@ -9,13 +18,24 @@ const Home: NextPage = () => {
         <title>Supply-chain Luxury Goods</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Product all products</span>
-          </h1>
-        </div>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        {/*  Site header */}
+        {/* <Header /> */}
+
+        {/*  Page content */}
+        <main className="flex-grow">
+          {/*  Page sections */}
+          <HeroHome />
+          <FeaturesHome />
+          <FeaturesBlocks />
+          <Testimonials />
+          <Newsletter />
+        </main>
+
+        <Banner />
+
+        {/*  Site footer */}
+        {/* <Footer /> */}
       </div>
     </>
   );
